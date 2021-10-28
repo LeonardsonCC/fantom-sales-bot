5807b84eb183f08588aa69f2502c4e1d30398554
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-10-28 07:26:08 -0300

    feat: trying to do Go working with api responses :confused:

---
+++ go.mod
@@ -0,0 +1,10 @@
+module leonardsoncc/twitter-bot
+
+go 1.16
+
+require (
+	github.com/dghubble/go-twitter v0.0.0-20211002212826-ad02880e616b // indirect
+	github.com/dghubble/oauth1 v0.7.0 // indirect
+	github.com/machinebox/graphql v0.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,133 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/machinebox/graphql"
 )
 
+type Sales []struct {
+	Id        string
+	Addresses []string
+	TokenIds  []string
+}
+
 func main() {
-  fmt.Println("Starting...")
+	fmt.Println("Starting...")
 
-  consumer_key := os.Getenv("CONSUMER_KEY")
-  consumer_secret := os.Getenv("CONSUMER_SECRET")
-  access_token := os.Getenv("ACCESS_TOKEN")
-  access_secret := os.Getenv("ACCESS_SECRET")
+	//twitterClient := getTwitterConfig()
 
-  fmt.Println(fmt.Sprintf("%v | %v | %v | %v", consumer_key, consumer_secret, access_token, access_secret))
+	client := graphql.NewClient("https://api.thegraph.com/subgraphs/name/paint-swap-finance/nftsv2")
+
+	recentSales := getRecentSales(client)
+	for _, sale := range recentSales.([]interface{}) {
+		fmt.Println(fmt.Sprintf("%v", sale))
+
+		address := sale.(map[string]interface{})["addresses"].([]interface{})[0].(string)
+		tokenId := sale.(map[string]interface{})["tokenIds"].([]interface{})[0].(string)
+		intTokenId, _ := strconv.ParseUint(tokenId, 10, 64)
+
+		history := getSaleHistory(address, uint(intTokenId), client)
+		fmt.Println(history)
+		// for _, historyItem := range history.([]interface{}) {
+		// 	fmt.Println(fmt.Sprintf("%v", historyItem))
+		// }
+	}
+}
+
+func getSaleHistory(contractAddress string, tokenId uint, client *graphql.Client) interface{} {
+	commonIndexes := make([]uint, 10)
+	var idsToSearch []string
+	for i := range commonIndexes {
+		idsToSearch = append(idsToSearch, fmt.Sprintf("\"%s_%d_%v\"", contractAddress, tokenId, i))
+	}
+
+	query := fmt.Sprintf(`
+		query {
+			nfthistories(where: {
+				id_in: [%v]
+				action: "Sold"
+			}) {
+				id
+				numericId
+				actionId
+				action
+				data
+				timestamp
+				hash
+			}
+		}
+	`, strings.Join(idsToSearch, ","))
+	// fmt.Println(query)
+
+	req := graphql.NewRequest(query)
+	ctx := context.Background()
+
+	var response map[string]interface{}
+
+	if err := client.Run(ctx, req, &response); err != nil {
+		log.Fatal(err)
+	}
+
+	return response
+}
+
+func getRecentSales(client *graphql.Client) interface{} {
+	query := fmt.Sprintf(`
+		query {
+			sales(
+				where: {
+					isERC721s_contains:[true]
+					endTime_gt: "%v"
+				} 
+				first: 5
+				orderDirection: desc 
+				orderBy: endTime
+			) {
+				id
+				addresses
+				tokenIds
+				endTime
+			}
+		}
+	`, 1637623400)
+	fmt.Println(query)
+
+	req := graphql.NewRequest(query)
+	ctx := context.Background()
+
+	var response map[string]interface{}
+
+	if err := client.Run(ctx, req, &response); err != nil {
+		log.Fatal(err)
+	}
+
+	return response["sales"]
+}
+
+func getTwitterConfig() *twitter.Client {
+	consumer_key, consumer_secret, access_token, access_secret := getTwitterCredentials()
+
+	config := oauth1.NewConfig(consumer_key, consumer_secret)
+	token := oauth1.NewToken(access_token, access_secret)
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	return client
+}
+
+func getTwitterCredentials() (string, string, string, string) {
+	consumer_key := os.Getenv("CONSUMER_KEY")
+	consumer_secret := os.Getenv("CONSUMER_SECRET")
+	access_token := os.Getenv("ACCESS_TOKEN")
+	access_secret := os.Getenv("ACCESS_SECRET")
+	return consumer_key, consumer_secret, access_token, access_secret
 }
