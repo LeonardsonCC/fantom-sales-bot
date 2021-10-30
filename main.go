40f1de91e98843024df6796a9797a1df24e9cefe
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-10-29 23:39:35 -0300

    feat: a lot, next thing to do is use last timestamp to next request

--- main.go
+++ main.go
@@ -14,10 +14,32 @@ import (
 	"github.com/machinebox/graphql"
 )
 
-type Sales []struct {
+type SaleHistoryItem struct {
+	TxHash   string
+	ActionId string
+	Value    float64
+	Time     int64
+}
+
+type SalesHistory []SaleHistoryItem
+
+func (a SalesHistory) Len() int {
+	return len(a)
+}
+func (a SalesHistory) Swap(i, j int64) {
+	a[i], a[j] = a[j], a[i]
+}
+func (a SalesHistory) Less(i, j int64) bool {
+	return a[i].Time < a[j].Time
+}
+
+type Sale struct {
 	Id        string
 	Addresses []string
 	TokenIds  []string
+	EndTime   int64
+	Value     float64
+	LastSales SalesHistory
 }
 
 func main() {
@@ -29,17 +51,40 @@ func main() {
 
 	recentSales := getRecentSales(client)
 	for _, sale := range recentSales.([]interface{}) {
-		fmt.Println(fmt.Sprintf("%v", sale))
+		//fmt.Println(fmt.Sprintf("%v", sale))
 
 		address := sale.(map[string]interface{})["addresses"].([]interface{})[0].(string)
 		tokenId := sale.(map[string]interface{})["tokenIds"].([]interface{})[0].(string)
 		intTokenId, _ := strconv.ParseUint(tokenId, 10, 64)
 
 		history := getSaleHistory(address, uint(intTokenId), client)
-		for _, historyItem := range history.([]interface{}) {
-			fmt.Println(fmt.Sprintf("%v", historyItem))
-			saleValue, _ := strconv.ParseFloat(historyItem.(map[string]interface{})["data"].(string), 32)
-			fmt.Printf("Valor: %.2f\n", bigIntToLegibleNumber(saleValue))
+		if len(history.([]interface{})) > 1 {
+			endTime, _ := strconv.ParseInt(sale.(map[string]interface{})["endTime"].(string), 10, 64)
+			price, _ := strconv.ParseFloat(sale.(map[string]interface{})["price"].(string), 64)
+			var sale *Sale = &Sale{
+				Id:        sale.(map[string]interface{})["id"].(string),
+				Addresses: []string{address},
+				TokenIds:  []string{tokenId},
+				EndTime:   endTime,
+				Value:     bigIntToLegibleNumber(price),
+			}
+			var salesHistory SalesHistory = make([]SaleHistoryItem, 0)
+
+			for _, historyItem := range history.([]interface{}) {
+				saleValue, _ := strconv.ParseFloat(historyItem.(map[string]interface{})["data"].(string), 32)
+				saleTime, _ := strconv.ParseInt(historyItem.(map[string]interface{})["timestamp"].(string), 10, 64)
+
+				var oldSale *SaleHistoryItem = &SaleHistoryItem{
+					TxHash:   historyItem.(map[string]interface{})["hash"].(string),
+					ActionId: historyItem.(map[string]interface{})["actionId"].(string),
+					Value:    bigIntToLegibleNumber(saleValue),
+					Time:     saleTime,
+				}
+				salesHistory = append(salesHistory, *oldSale)
+			}
+			sale.LastSales = salesHistory
+
+			fmt.Println(fmt.Sprintf("%v", sale))
 		}
 	}
 }
@@ -92,6 +137,8 @@ func getRecentSales(client *graphql.Client) interface{} {
 					isERC721s_contains:[true]
 					endTime_gt: "%v"
 					addresses: ["0xfd211f3b016a75bc8d73550ac5adc2f1cae780c0"]
+          complete: true
+          sold: true
 				} 
 				first: 100
 				orderDirection: desc 
@@ -101,6 +148,7 @@ func getRecentSales(client *graphql.Client) interface{} {
 				addresses
 				tokenIds
 				endTime
+        price
 			}
 		}
 	`, 1631365200)
