634589c4b8700dcb937b6525e774d72089504480
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-11-04 21:10:51 -0300

    feat(sales): maybe the final first version!

--- main.go
+++ main.go
@@ -62,80 +62,24 @@ type FantomPriceResponse struct {
 	MarketData MarketData `json:"market_data"`
 }
 
+var currentTime time.Time = time.Now()
+
 func main() {
 	fmt.Println("Starting...")
 
-	//twitterClient := getTwitterConfig()
+	for {
+		twitterClient := getTwitterConfig()
 
-	client := graphql.NewClient("https://api.thegraph.com/subgraphs/name/paint-swap-finance/nftsv2")
+		client := graphql.NewClient("https://api.thegraph.com/subgraphs/name/paint-swap-finance/nftsv2")
 
-	recentSales := getRecentSales(client)
-	for _, sale := range recentSales.([]interface{}) {
-		address := sale.(map[string]interface{})["addresses"].([]interface{})[0].(string)
-		tokenId := sale.(map[string]interface{})["tokenIds"].([]interface{})[0].(string)
-		intTokenId, _ := strconv.ParseUint(tokenId, 10, 64)
+		recentSales := getRecentSales(client)
+		for _, sale := range recentSales.([]interface{}) {
+			address := sale.(map[string]interface{})["addresses"].([]interface{})[0].(string)
+			tokenId := sale.(map[string]interface{})["tokenIds"].([]interface{})[0].(string)
 
-		history := getSaleHistory(address, uint(intTokenId), client)
-		if len(history.([]interface{})) > 1 {
-			endTime, _ := strconv.ParseInt(sale.(map[string]interface{})["endTime"].(string), 10, 64)
-			price, _ := strconv.ParseFloat(sale.(map[string]interface{})["price"].(string), 64)
-			var sale *Sale = &Sale{
-				Id:        sale.(map[string]interface{})["id"].(string),
-				Addresses: []string{address},
-				TokenIds:  []string{tokenId},
-				EndTime:   endTime,
-				Value:     bigIntToLegibleNumber(price),
-			}
-			var salesHistory SalesHistory = make([]SaleHistoryItem, 0)
-
-			for _, historyItem := range history.([]interface{}) {
-				saleValue, _ := strconv.ParseFloat(historyItem.(map[string]interface{})["data"].(string), 32)
-				saleTime, _ := strconv.ParseInt(historyItem.(map[string]interface{})["timestamp"].(string), 10, 64)
-
-				var oldSale *SaleHistoryItem = &SaleHistoryItem{
-					TxHash:   historyItem.(map[string]interface{})["hash"].(string),
-					ActionId: historyItem.(map[string]interface{})["actionId"].(string),
-					Value:    bigIntToLegibleNumber(saleValue),
-					Time:     saleTime,
-				}
-				salesHistory = append(salesHistory, *oldSale)
-			}
-			sale.LastSales = salesHistory
-
-			var boughtAction SaleHistoryItem = salesHistory[len(sale.LastSales)-2]
-			var soldAction SaleHistoryItem = salesHistory[len(sale.LastSales)-1]
-
-			var tweetMessage string = ""
-
-			boughtFantomPrice := getFantomPrice(time.Unix(boughtAction.Time, 0).Format(layoutISO))
-			soldFantomPrice := getFantomPrice(time.Unix(soldAction.Time, 0).Format(layoutISO))
-
-			tweetMessage += fmt.Sprintf("🛍 Bought: %v FTM @ $%.2f\n", boughtAction.Value, boughtFantomPrice)
-			tweetMessage += fmt.Sprintf("💰 Sold: %v FTM @ $%.2f\n", soldAction.Value, soldFantomPrice)
-
-			boughtAt := boughtAction.Value
-			soldAt := soldAction.Value
-			difference := soldAt - boughtAt
-			differenceDollar := (soldAt * soldFantomPrice) - (boughtAt * boughtFantomPrice)
-
-			dateDifference := time.Unix(soldAction.Time, 0).Sub(time.Unix(boughtAction.Time, 0))
-			tweetMessage += fmt.Sprintf("🤝 HODL duration: %.0f days\n", dateDifference.Hours()/24)
-
-			if difference > 0 {
-				tweetMessage += fmt.Sprintf("📈 Gain: %v FTM\n", difference)
-			} else {
-				tweetMessage += fmt.Sprintf("📉 Loss: %v FTM\n", (difference * -1))
-			}
-
-			if differenceDollar > 0 {
-				tweetMessage += fmt.Sprintf("💵 Profit: $%.2f (📈 %.2f%%)\n", differenceDollar, ((difference / boughtAt) * 100))
-			} else {
-				tweetMessage += fmt.Sprintf("💵 Loss: $%.2f (📈 %.2f%%)\n", (differenceDollar * -1), ((difference / boughtAt) * 100))
-			}
-			tweetMessage += fmt.Sprintf("https://paintswap.finance/marketplace/%v", soldAction.ActionId)
-
-			fmt.Println(tweetMessage + "\n")
+			go fetchSaleHistoryAndTweet(twitterClient, client, sale, tokenId, address)
 		}
+		time.Sleep(time.Minute * 5)
 	}
 }
 
@@ -160,7 +104,80 @@ func getFantomPrice(dateString string) float64 {
 	var response FantomPriceResponse
 	json.Unmarshal(body, &response)
 
+	if response.MarketData.CurrentPrice.Usd == 0 {
+		fmt.Println("Coingeck returned 0 in USD price... Trying again")
+		return getFantomPrice(dateString)
+	}
+
 	return response.MarketData.CurrentPrice.Usd
+}
+
+func fetchSaleHistoryAndTweet(twitterClient *twitter.Client, client *graphql.Client, sale interface{}, tokenId string, address string) {
+	intTokenId, _ := strconv.ParseUint(tokenId, 10, 64)
+	history := getSaleHistory(address, uint(intTokenId), client)
+	if len(history.([]interface{})) > 1 {
+		endTime, _ := strconv.ParseInt(sale.(map[string]interface{})["endTime"].(string), 10, 64)
+		price, _ := strconv.ParseFloat(sale.(map[string]interface{})["price"].(string), 64)
+		var sale *Sale = &Sale{
+			Id:        sale.(map[string]interface{})["id"].(string),
+			Addresses: []string{address},
+			TokenIds:  []string{tokenId},
+			EndTime:   endTime,
+			Value:     bigIntToLegibleNumber(price),
+		}
+		var salesHistory SalesHistory = make([]SaleHistoryItem, 0)
+
+		for _, historyItem := range history.([]interface{}) {
+			saleValue, _ := strconv.ParseFloat(historyItem.(map[string]interface{})["data"].(string), 32)
+			saleTime, _ := strconv.ParseInt(historyItem.(map[string]interface{})["timestamp"].(string), 10, 64)
+
+			var oldSale *SaleHistoryItem = &SaleHistoryItem{
+				TxHash:   historyItem.(map[string]interface{})["hash"].(string),
+				ActionId: historyItem.(map[string]interface{})["actionId"].(string),
+				Value:    bigIntToLegibleNumber(saleValue),
+				Time:     saleTime,
+			}
+			salesHistory = append(salesHistory, *oldSale)
+		}
+		sale.LastSales = salesHistory
+
+		var boughtAction SaleHistoryItem = salesHistory[len(sale.LastSales)-2]
+		var soldAction SaleHistoryItem = salesHistory[len(sale.LastSales)-1]
+
+		var tweetMessage string = ""
+
+		tweetMessage += fmt.Sprintf("🧾 Contract: %v\n\n", strings.Join(sale.Addresses, ","))
+
+		boughtFantomPrice := getFantomPrice(time.Unix(boughtAction.Time, 0).Format(layoutISO))
+		soldFantomPrice := getFantomPrice(time.Unix(soldAction.Time, 0).Format(layoutISO))
+
+		tweetMessage += fmt.Sprintf("🛍 Bought: %v FTM @ $%.2f\n", boughtAction.Value, boughtFantomPrice)
+		tweetMessage += fmt.Sprintf("💰 Sold: %v FTM @ $%.2f\n", soldAction.Value, soldFantomPrice)
+
+		boughtAt := boughtAction.Value
+		soldAt := soldAction.Value
+		difference := soldAt - boughtAt
+		differenceDollar := (soldAt * soldFantomPrice) - (boughtAt * boughtFantomPrice)
+
+		dateDifference := time.Unix(soldAction.Time, 0).Sub(time.Unix(boughtAction.Time, 0))
+		tweetMessage += fmt.Sprintf("🤝 HODL duration: %.0f days\n", dateDifference.Hours()/24)
+
+		if difference > 0 {
+			tweetMessage += fmt.Sprintf("📈 Gain: %v FTM\n", difference)
+		} else {
+			tweetMessage += fmt.Sprintf("📉 Loss: %v FTM\n", (difference * -1))
+		}
+
+		if differenceDollar > 0 {
+			tweetMessage += fmt.Sprintf("💵 Profit: $%.2f (📈 %.2f%%)\n", differenceDollar, ((difference / boughtAt) * 100))
+		} else {
+			tweetMessage += fmt.Sprintf("💵 Loss: $%.2f (📈 %.2f%%)\n", (differenceDollar * -1), ((difference / boughtAt) * 100))
+		}
+		tweetMessage += fmt.Sprintf("https://paintswap.finance/marketplace/%v", soldAction.ActionId)
+
+		fmt.Println(tweetMessage + "\n")
+		twitterClient.Statuses.Update(tweetMessage, nil)
+	}
 }
 
 func getSaleHistory(contractAddress string, tokenId uint, client *graphql.Client) interface{} {
@@ -206,7 +223,6 @@ func getRecentSales(client *graphql.Client) interface{} {
 				where: {
 					isERC721s_contains:[true]
 					endTime_gt: "%v"
-					addresses: ["0xfd211f3b016a75bc8d73550ac5adc2f1cae780c0"]
           complete: true
           sold: true
 				} 
@@ -221,7 +237,7 @@ func getRecentSales(client *graphql.Client) interface{} {
         price
 			}
 		}
-	`, 1631365200)
+    `, currentTime.Unix()) // TODO: remove hardcoded time and contract to use the last update time
 
 	req := graphql.NewRequest(query)
 	ctx := context.Background()
@@ -231,6 +247,8 @@ func getRecentSales(client *graphql.Client) interface{} {
 	if err := client.Run(ctx, req, &response); err != nil {
 		log.Fatal(err)
 	}
+
+	currentTime = currentTime.Add(time.Minute * time.Duration(5))
 
 	return response["sales"]
 }
