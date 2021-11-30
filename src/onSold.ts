c374e537ea4c05e7d07ca513e31a1ea542c2b4ce
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-11-30 08:19:56 -0300

    feat: added mint price

--- src/onSold.ts
+++ src/onSold.ts
@@ -1,6 +1,8 @@
 import { Sold } from "@paintswap/marketplace-interactions";
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
+import { isConstValueNode } from "graphql";
 import { fetchPrice } from "./providers/coingecko";
+import { fetchContractTx } from "./providers/ftmscan";
 import { fetchContractName } from "./providers/generic-contract";
 import { fetchItemHistory, HistoryItem } from "./providers/ps-api";
 
@@ -35,6 +37,48 @@ const onSold = async (
   let lastSale: null | HistoryItem = null;
   if (history.length > 1) {
     lastSale = history.slice(-1)[0];
+  } else {
+    const txs = await fetchContractTx(sale.collection);
+    const sellerTxs = txs.filter((tx) => {
+      return (
+        tx.from.toLowerCase() === sale.seller.toLowerCase() &&
+        tx.contractAddress === "" &&
+        BigNumber.from(tx.value).gt(0)
+      );
+    });
+
+    let foundIt = false;
+    for (const tx of sellerTxs) {
+      if (foundIt) break;
+      const receipt = await provider.getTransactionReceipt(tx.hash);
+
+      for (const log of receipt.logs) {
+        if (foundIt) break;
+
+        if (log.topics.length === 4) {
+          if (
+            log.topics.filter((receipt) =>
+              BigNumber.from(receipt).eq(sale.tokenID)
+            ).length
+          ) {
+            // Probably the mint event
+            foundIt = true;
+            lastSale = {
+              action: "0",
+              actionId: "0",
+              data: BigNumber.from(tx.value).toString(),
+              hash: tx.hash,
+              id: "0",
+              numericId: "0",
+              timestamp: tx.timeStamp,
+              version: 2,
+            };
+            break;
+          }
+        }
+      }
+    }
+    console.log("finished", sale, lastSale);
   }
 
   try {
@@ -50,6 +94,8 @@ const onSold = async (
 
   if (lastSale) {
     try {
+      if (lastSale.timestamp.length === 10)
+        lastSale.timestamp = `${lastSale.timestamp}000`;
       const {
         market_data: {
           current_price: { usd: lastSalePrice },
---
+++ src/providers/ftmscan.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosResponse } from "axios";
+
+interface IFTMScanTx {
+  blockNumber: string;
+  timeStamp: string;
+  hash: string;
+  nonce: string;
+  blockHash: string;
+  transactionIndex: string;
+  from: string;
+  to: string;
+  value: string;
+  gas: string;
+  gasPrice: string;
+  isError: string;
+  txreceipt_status: string;
+  input: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  gasUsed: string;
+  confirmations: string;
+}
+
+const fetchContractTx = async (
+  contractAddress: string
+): Promise<IFTMScanTx[]> => {
+  const { data } = await axios.get(
+    `https://api.ftmscan.com/api?module=account&action=txlist&address=${contractAddress}&startblock=0&endblock=99999999&sort=asc&apikey=${process.env.FTMSCAN_API_KEY}`
+  );
+
+  return data.result;
+};
+
+export { fetchContractTx };
