61044f58df058460414ff49316414841c964fcbe
LeonardsonCC <leonardson.carvalho@gmail.com>
2021-11-30 13:04:02 -0300

    fix: try again till get some results from coingecko

--- src/providers/coingecko.ts
+++ src/providers/coingecko.ts
@@ -18,9 +18,18 @@ const fetchPrice = async (
     "0"
   )}-${String(time.getMonth()).padStart(2, "0")}-${time.getFullYear()}`;
 
-  const { data } = await axios.get(
-    `https://api.coingecko.com/api/v3/coins/${coin}/history?date=${timeString}`
-  );
+  let data;
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${coin}/history?date=${timeString}`
+    );
+    data = response.data;
+  } catch (err) {
+    setTimeout(() => {
+      console.log("Error fetching price... Waiting 5s and trying again");
+      fetchPrice(coin, timestamp);
+    }, 5000);
+  }
 
   return data;
 };
